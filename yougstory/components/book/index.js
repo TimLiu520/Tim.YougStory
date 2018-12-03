f6156aa3487759420ddbfd37da9464c94de508f9
TimLiu520 <350040493@qq.com>
2018-12-03 22:18:57 +0800

    书籍详情

--- yougstory/components/book/index.js
+++ yougstory/components/book/index.js
@@ -4,7 +4,8 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    book:Object
+    book:Object,
+    href:String
   },
 
   /**
@@ -19,5 +20,12 @@ Component({
    */
   methods: {
 
+    onTab(event)
+    {
+        const bid=this.properties.book.id;
+        wx.navigateTo({
+            url:this.properties.href
+        });
+    }
   }
 })
--- yougstory/models/book.js
+++ yougstory/models/book.js
@@ -4,5 +4,27 @@ class BookModel extends HTTP{
         return this.request({url:'book/hot_list'});
         //return this.request({ url= 'book/hot_list'});
     }
+
+    getMyBookCount(){
+        return this.request({url:'/book/favor/count'});
+        //return this.request({ url= 'book/hot_list'});
+    }
+
+    getDetail(bid){
+        return this.request({url:'book/'+bid+'/detail'});
+        //return this.request({ url= 'book/hot_list'});
+    }
+
+    getLikeStatus(bid){
+        return this.request({url:'book/'+bid+'/favor'});
+        //return this.request({ url= 'book/hot_list'});
+    }
+
+    getComments(bid){
+        return this.request({url:'book/'+bid+'/short_comment'});
+        //return this.request({ url= 'book/hot_list'});
+    }
+
+    
 }
 export {BookModel}
